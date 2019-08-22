ed7bdc26de0e4d5b78522fbd837c24eacf7a3c8b
Rosinskyi <sergey22061993@gmail.com>
2019-08-22 12:00:35 +0300

    added solution sor jv-sqrt-exception

--- src/main/java/core/basesyntax/SqrtOperation.java
+++ src/main/java/core/basesyntax/SqrtOperation.java
@@ -8,9 +8,14 @@ package core.basesyntax;
  */
 public class SqrtOperation {
     public double sqrt(double number) {
-        if (number < 0) {
-            throw new IllegalArgumentException("Expected non-negative number, got " + number);
+        try {
+            if (number < 0) {
+                throw new IllegalArgumentException();
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println("Expected non-negative number, got " + number);
         }
         return Math.sqrt(number);
     }
+
 }
