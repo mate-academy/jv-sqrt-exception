01007eae91868bcb69e54837fbdf28983200ded1
lenovo <suleimanovayev@gmail.com>
2019-08-22 15:09:34 +0300

    task exception sqrt

--- src/main/java/core/basesyntax/SqrtOperation.java
+++ src/main/java/core/basesyntax/SqrtOperation.java
@@ -7,7 +7,11 @@ package core.basesyntax;
  * где вместо вопросика будет подставлено фактически переданное в метод число.</p>
  */
 public class SqrtOperation {
-    public double sqrt(double number) {
-        return 0.0;
+    public double sqrt(double number) throws IllegalArgumentException {
+        if (number < 0) {
+            throw new IllegalArgumentException("Expected non-negative number, got " + number);
+        }
+        return Math.sqrt(number);
     }
+
 }
