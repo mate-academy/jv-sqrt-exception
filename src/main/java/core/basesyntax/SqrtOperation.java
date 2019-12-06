1cfb1c9b83ec604106ffac73095b175fe64dc826
Serhii Blakh <s.a.blakh@ukr.net>
2019-12-06 13:25:06 +0200

    added solution homework_4.1

--- src/main/java/core/basesyntax/SqrtOperation.java
+++ src/main/java/core/basesyntax/SqrtOperation.java
@@ -8,16 +8,9 @@ package core.basesyntax;
  */
 public class SqrtOperation {
     public double sqrt(double number) {
-        double sqrtNew = 0;
         if (number < 0) {
             throw new IllegalArgumentException("Expected non-negative number, got " + number);
         }
-        int temp = 1;
-        while (number != 0) {
-            number = number - temp;
-            sqrtNew++;
-            temp += 2;
-        }
-        return sqrtNew;
+        return Math.sqrt(number);
     }
 }
