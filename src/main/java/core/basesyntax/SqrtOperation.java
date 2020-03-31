ff407a17af6b0e7e37c254132ebea1806bd75b98
unknown <bavikaalexey@gmail.com>
2020-03-31 21:59:32 +0300

    added solution

--- src/main/java/core/basesyntax/SqrtOperation.java
+++ src/main/java/core/basesyntax/SqrtOperation.java
@@ -6,8 +6,13 @@ package core.basesyntax;
  * java.lang.IllegalArgumentException с сообщением "Expected non-negative number, got ?",
  * где вместо вопросика будет подставлено фактически переданное в метод число.</p>
  */
+
 public class SqrtOperation {
+
     public double sqrt(double number) {
-        return 0.0;
+        if (number < 0) {
+            throw new IllegalArgumentException("Expected non-negative number, got " + number);
+        }
+        return Math.sqrt(number);
     }
 }
