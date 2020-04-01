3cca9220931a3145096dafd398dfb81be39230a8
Andrey Tanchak <tanchak12@gmail.com>
2020-04-01 15:01:40 +0300

    added solution

--- src/main/java/core/basesyntax/SqrtOperation.java
+++ src/main/java/core/basesyntax/SqrtOperation.java
@@ -1,13 +1,16 @@
 package core.basesyntax;
 
-/**
- * <p>Реализуйте метод sqrt(), вычисляющий квадратный корень числа. В отличие от Math.sqrt(),
- * это метод при передаче отрицательного параметра должен бросать исключение
- * java.lang.IllegalArgumentException с сообщением "Expected non-negative number, got ?",
- * где вместо вопросика будет подставлено фактически переданное в метод число.</p>
- */
 public class SqrtOperation {
-    public double sqrt(double number) {
-        return 0.0;
+    public double sqrt(double number) throws java.lang.IllegalArgumentException {
+        if (number >= 0) {
+            int a = 0;
+            while (Math.pow(a, 2) != number) {
+                a++;
+            }
+            return a;
+        } else {
+            throw new IllegalArgumentException("Expected non-negative number, got " + number);
+        }
     }
 }
+
