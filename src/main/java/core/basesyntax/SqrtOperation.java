6e43e3360fa75dc48e8a3117b717f651407fea05
Kseniia Makarova <kseniia.makarova.kyiv@gmail.com>
2020-04-01 13:48:18 +0300

    added solution of hw-3.1

--- src/main/java/core/basesyntax/SqrtOperation.java
+++ src/main/java/core/basesyntax/SqrtOperation.java
@@ -8,6 +8,23 @@ package core.basesyntax;
  */
 public class SqrtOperation {
     public double sqrt(double number) {
-        return 0.0;
+        if (number < 0) {
+            throw new IllegalArgumentException("Expected non-negative number, got " + number);
+        }
+        double low = 0;
+        double high = number;
+        double mid = 0;
+        for (int i = 0; i < 100; i++) {
+            mid = (low + high) / 2;
+            if (mid * mid == number) {
+                return mid;
+            }
+            if (mid * mid < number) {
+                low = mid;
+            } else {
+                high = mid;
+            }
+        }
+        return mid;
     }
 }
