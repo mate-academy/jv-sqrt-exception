7e10f596f6f68c207e614c219c8c9a9bb7767fad
Popovych Yurii <popovychyb@gmail.com>
2020-04-01 15:15:54 +0300

    deleted redundant symbol from the error message

--- src/main/java/core/basesyntax/SqrtOperation.java
+++ src/main/java/core/basesyntax/SqrtOperation.java
@@ -9,7 +9,7 @@ package core.basesyntax;
 public class SqrtOperation {
     public double sqrt(double number) {
         if (number < 0) {
-            throw new IllegalArgumentException("Expected non-negative number, got? " + number);
+            throw new IllegalArgumentException("Expected non-negative number, got " + number);
         }
         return Math.sqrt(number);
     }
