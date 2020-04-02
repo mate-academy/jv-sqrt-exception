5347d4b026f04f40c54f37ea1540e9a477ac788e
Andrey Tanchak <61573898+boterf@users.noreply.github.com>
2020-04-02 10:42:57 +0300

    Update SqrtOperation.java

--- src/main/java/core/basesyntax/SqrtOperation.java
+++ src/main/java/core/basesyntax/SqrtOperation.java
@@ -4,10 +4,9 @@ public class SqrtOperation {
     public double sqrt(double number) throws java.lang.IllegalArgumentException {
         if (number < 0) {
             throw new IllegalArgumentException("Expected non-negative number, got " + number); 
-        } else {
+        }
             number = Math.sqrt(number);
             return number;
-        }
     }
 }
 
