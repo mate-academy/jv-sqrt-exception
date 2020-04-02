70365dba508e2fad89bf2633f6e96206408af4f9
Andrey Tanchak <tanchak12@gmail.com>
2020-04-02 10:50:46 +0300

    CHECKED

--- src/main/java/core/basesyntax/SqrtOperation.java
+++ src/main/java/core/basesyntax/SqrtOperation.java
@@ -1,7 +1,7 @@
 package core.basesyntax;
 
 public class SqrtOperation {
-  public double sqrt(double number) throws java.lang.IllegalArgumentException {
+    public double sqrt(double number) throws java.lang.IllegalArgumentException {
         if (number < 0) {
             throw new IllegalArgumentException("Expected non-negative number, got " + number);
         }
