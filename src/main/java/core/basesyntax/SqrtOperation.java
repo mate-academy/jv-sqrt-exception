f31468decda7864a492bda9c1fea770095509ad8
Andriy <andriy.lagun@gmail.com>
2020-07-27 22:52:32 +0300

    Implemented sqrt

--- src/main/java/core/basesyntax/SqrtOperation.java
+++ src/main/java/core/basesyntax/SqrtOperation.java
@@ -8,6 +8,8 @@ package core.basesyntax;
  */
 public class SqrtOperation {
     public double sqrt(double number) {
-        return 0.0;
-    }
+        if(number<0)
+            throw new IllegalArgumentException("Expected non-negative number, got " + number);
+        return Math.sqrt(number);
+        }
 }
