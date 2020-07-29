1463f9915da5de2f19064d92c780f59af103da70
Daria <dariyapikul@gmail.com>
2020-07-29 13:32:45 +0300

    solution added

--- src/main/java/core/basesyntax/SqrtOperation.java
+++ src/main/java/core/basesyntax/SqrtOperation.java
@@ -7,7 +7,12 @@ package core.basesyntax;
  * где вместо вопросика будет подставлено фактически переданное в метод число.</p>
  */
 public class SqrtOperation {
-    public double sqrt(double number) {
-        return 0.0;
+
+    public double sqrt(double number) throws java.lang.IllegalArgumentException {
+        if (number < 0) {
+            throw new IllegalArgumentException("Expected non-negative number, got " + number);
+        }
+        return Math.pow(number, 0.5);
     }
+
 }
