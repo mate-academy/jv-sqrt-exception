e32edf3609c19c18d8c218c6fbe3fa2083d70d3e
Viktor Nikolaev <nikolaev.vikt@gmail.com>
2020-07-29 14:14:16 +0300

    [ADD] solution hw-3.1

--- src/main/java/core/basesyntax/SqrtOperation.java
+++ src/main/java/core/basesyntax/SqrtOperation.java
@@ -7,7 +7,11 @@ package core.basesyntax;
  * где вместо вопросика будет подставлено фактически переданное в метод число.</p>
  */
 public class SqrtOperation {
-    public double sqrt(double number) {
-        return 0.0;
+    public double sqrt(double number){
+        if (number < 0.0) {
+            throw new java.lang.IllegalArgumentException();
+        }
+
+        return Math.sqrt(number);
     }
 }
