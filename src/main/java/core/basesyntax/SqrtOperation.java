7ae8ac8f8a656b44c1ddca1123c1a2dd8dd0aa52
m.mikhajlov <lizappy31@gmail.com>
2020-07-29 17:13:33 +0300

    changed symbol

--- src/main/java/core/basesyntax/SqrtOperation.java
+++ src/main/java/core/basesyntax/SqrtOperation.java
@@ -10,7 +10,7 @@ public class SqrtOperation {
     public double sqrt(double number) {
         if (number < 0) {
             throw new IllegalArgumentException(
-                    String.format("Expected non-negative number, got ?", number));
+                    String.format("Expected non-negative number, got %d", number));
         }
         return Math.sqrt(number);
     }
