a675fbe25782933c4c677faaacc7776a7883f66a
Your Name <i.dymytreyeva@gmail.com>
2020-07-29 12:47:01 +0300

    solution added

--- src/main/java/core/basesyntax/SqrtOperation.java
+++ src/main/java/core/basesyntax/SqrtOperation.java
@@ -8,6 +8,15 @@ package core.basesyntax;
  */
 public class SqrtOperation {
     public double sqrt(double number) {
-        return 0.0;
+        double sqrtNumber = 0;
+        try {
+            sqrtNumber = Math.sqrt(number);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Expected non-negative number, got " + number);
+        }
+        if (number < 0) {
+            throw new IllegalArgumentException("Expected non-negative number, got " + number);
+        }
+        return sqrtNumber;
     }
 }
