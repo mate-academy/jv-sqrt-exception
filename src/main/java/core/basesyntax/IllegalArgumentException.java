1d329fce76c54e420b83178246244860a71c4c65
Artem Vremere <vremere_a@ukr.net>
2020-07-30 11:47:00 +0300

    mvn checked

--- src/main/java/core/basesyntax/IllegalArgumentException.java
+++ src/main/java/core/basesyntax/IllegalArgumentException.java
@@ -1,6 +1,6 @@
 package core.basesyntax;
 
- class IllegalArgumentException extends RuntimeException {
+class IllegalArgumentException extends RuntimeException {
     public IllegalArgumentException(String message) {
         super(message);
     }
